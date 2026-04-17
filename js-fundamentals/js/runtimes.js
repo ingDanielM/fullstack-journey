d6924968d039d7f8953400ad286deb5edc8fb351
ingDanielM <danieljosem097@gmail.com>
2026-04-17 05:29:24 -0500

    runtimes

---
+++ js-fundamentals/js/runtimes.js
@@ -0,0 +1,12 @@
+// Runtimes ***********************************************************************************************
+console.clear();
+// A JavaScript runtime is the environment where JavaScript code runs.
+// It provides APIs, executes the code, and manages features like the event loop.
+
+// Node.js ***********************************************************************************************
+// is a JavaScript runtime that allows you to run JavaScript outside the browser,
+// mainly for backend development, scripts, and tools.
+
+// NPM ***********************************************************************************************
+// npm (Node Package Manager) is a tool used to install, manage,
+// and share JavaScript packages and dependencies.
